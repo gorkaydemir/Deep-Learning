dd45db3303145cc4c304c5ac0ce539b661173b2b
Görkay Aydemir <57246332+gorkaydemir@users.noreply.github.com>
2020-12-26 20:49:50 +0300

    Add files via upload

---
+++ nn_one_hl.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 26 20:39:41 2020
+
+@author: gorkay
+"""
+import numpy as np
+
+
+def sigmoid(z):
+    s = 1/(1+np.exp(-z))    
+    return s
+
+shape_X = X.shape
+shape_Y = Y.shape
+m = X.shape[1]  # training set size
+
+def layer_sizes(X, Y):
+    n_x = X.shape[0] # size of input layer
+    n_h = 4
+    n_y = Y.shape[0] # size of output layer
+    return (n_x, n_h, n_y)
+
+def initialize_parameters(n_x, n_h, n_y):
+    np.random.seed(2)
+    
+    W1 = np.random.randn(n_h,n_x) * 0.01
+    b1 = np.zeros((n_h,1))
+    W2 = np.random.randn(n_y,n_h) * 0.01
+    b2 = np.zeros((n_y,1))
+    
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def forward_propagation(X, parameters):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    
+    assert(A2.shape == (1, X.shape[1]))
+    
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    
+    return A2, cache
+
+
+def compute_cost(A2, Y, parameters):
+    m = Y.shape[1]
+
+    logprobs = (Y*np.log(A2) + (1-Y)*np.log(1-A2))
+    cost = - np.sum(logprobs)/m
+    
+    cost = float(np.squeeze(cost))
+    assert(isinstance(cost, float))
+    
+    return cost
+
+def backward_propagation(parameters, cache, X, Y):
+    m = X.shape[1]
+
+    W2 = parameters["W2"]
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+
+    dZ2 = A2 - Y
+    dW2 = np.dot(dZ2, A1.T)/m
+    db2 = np.sum(dZ2, axis = 1, keepdims = True)/m
+    dZ1 = np.dot(W2.T, dZ2)*(1 - np.power(A1, 2))
+    dW1 = np.dot(dZ1, X.T)/m
+    db1 = np.sum(dZ1, axis = 1, keepdims = True)/m
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate = 1.2):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    dW1 = grads["dW1"]
+    db1 = grads["db1"]
+    dW2 = grads["dW2"]
+    db2 = grads["db2"]
+
+    W1 = W1 - learning_rate*dW1
+    b1 = b1 - learning_rate*db1
+    W2 = W2 - learning_rate*dW2
+    b2 = b2 - learning_rate*db2
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
+
+    np.random.seed(3)
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+    
+    parameters = initialize_parameters(n_x, n_h, n_y)
+
+    for i in range(0, num_iterations):
+        
+        A2, cache = forward_propagation(X, parameters)
+        
+        cost = compute_cost(A2, Y, parameters)
+ 
+        grads = backward_propagation(parameters, cache, X, Y)
+ 
+        parameters = update_parameters(parameters, grads)
+        
+        if print_cost and i % 1000 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+
+    return parameters
+
+def predict(parameters, X):
+    A2, cache = forward_propagation(X, parameters)
+    predictions = 1*(A2 > 0.5)
+    
+    return predictions
